a0c11ff396609f2a5e387e8a174ffc32918ccf6c
MiDo1806 <81757415+MiDo1806@users.noreply.github.com>
2021-12-07 04:23:58 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const greenColor = 'green';
+const yellowColor = 'yellow';
+const directionWall = 'south';
+
+
+
+const paintWall = function (item) {
+    console.log('Paint this wall ' + item);
+
+    console.log('Paint your ' + directionWall + 'wal ' + yellowColor);
+}
+
+
+paintWall(greenColor);
+
